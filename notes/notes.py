7303189c3d1264d38889d69f9cf498b65484b870
sbdiv <143803108+sbdiv@users.noreply.github.com>
2023-12-18 22:18:05 +0200

    modified:   notes/notes.py

--- notes/notes.py
+++ notes/notes.py
@@ -1,5 +1,3 @@
-from prettytable import PrettyTable
-from note_record import NoteRecord
 
 class Notes:
 
@@ -8,28 +6,18 @@ class Notes:
         self.notes = []
 
     # додати нотатку
-    def add_note(self, author, text, tags):
-        new_note =  NoteRecord(author, text, tags)
-        self.notes.append(new_note)
+    def add_note(self, note):
+        self.notes.append(note)
 
     # пошук за тегами
     def search_by_tags(self, search_tags):
         result_notes = [note for note in self.notes if set(search_tags).issubset(note.tags)]
-        if result_notes:
-            table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
-            for i, note in enumerate(result_notes):
-                table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
-            print(table)
-        else:
-            print("No notes found with the specified tags.")
+        return result_notes
 
     # сортування за тегами по алфавіту
     def sort_by_tags(self):
         sorted_notes = sorted(self.notes, key=lambda note: ','.join(note.tags))
-        table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
-        for i, note in enumerate(sorted_notes):
-            table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
-        print(table)
+        return sorted_notes
 
     # видалення нотатки
     def delete_note_by_index(self, index):
@@ -39,10 +27,3 @@ class Notes:
         else:
             print("Invalid note index.")
 
-    # Виведення нотаток таблицею
-    def display_notes_table(self):
-            table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
-            for i, note in enumerate(self.notes):
-                table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
-
-            print(table)
