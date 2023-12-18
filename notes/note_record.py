6ddc616ff1d37f3234d4ac8cfe138e1865434927
sbdiv <143803108+sbdiv@users.noreply.github.com>
2023-12-18 22:21:06 +0200

    modified:   notes/note_record.py modified:   notes/notes.py

--- notes/note_record.py
+++ notes/note_record.py
@@ -8,15 +8,12 @@ class NoteRecord:
         self.tags = tags
         self.date = date if date else datetime.datetime.now()
 
-    # зміна тексту нотатки
     def update_text(self, new_text):
         self.text = new_text
 
-    # зміна автора
     def update_author(self, new_author):
             self.author = new_author
 
-    # зміна тегу
     def update_tags(self, new_tags):
         self.tags = new_tags
 
--- notes/notes.py
+++ notes/notes.py
@@ -1,25 +1,20 @@
 
 class Notes:
 
-    # список для зберігання нотаток
     def __init__(self):
         self.notes = []
 
-    # додати нотатку
     def add_note(self, note):
         self.notes.append(note)
 
-    # пошук за тегами
     def search_by_tags(self, search_tags):
         result_notes = [note for note in self.notes if set(search_tags).issubset(note.tags)]
         return result_notes
 
-    # сортування за тегами по алфавіту
     def sort_by_tags(self):
         sorted_notes = sorted(self.notes, key=lambda note: ','.join(note.tags))
         return sorted_notes
 
-    # видалення нотатки
     def delete_note_by_index(self, index):
         if 0 <= index < len(self.notes):
             del self.notes[index]
