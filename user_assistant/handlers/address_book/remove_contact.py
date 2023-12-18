31b23dc1b5f67cb74e8087a9d526ec02d99c30a5
PotapovALeksey <37796979+PotapovALeksey@users.noreply.github.com>
2023-12-18 20:47:19 +0200

    fix

--- user_assistant/handlers/address_book/remove_contact.py
+++ user_assistant/handlers/address_book/remove_contact.py
@@ -1,9 +1,9 @@
 from typing import Type
 
-from address_book.address_book import AddressBook
-from class_fields.name import Name
-from handlers.address_book.add_contact import input_value
-from storages.storage import Storage
+from user_assistant.address_book.address_book import AddressBook
+from user_assistant.class_fields.name import Name
+from user_assistant.handlers.address_book.add_contact import input_value
+from user_assistant.storages.storage import Storage
 
 def remove_contact(book: AddressBook, storage: Type[Storage]):
     name = input_value('name', Name)
