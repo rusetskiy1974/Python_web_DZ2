907abf0d073db9cb88bb10aae2707b8bc7f03579
sbdiv <143803108+sbdiv@users.noreply.github.com>
2023-12-18 21:48:38 +0200

    modified:   notes/note_record.py 	modified:   notes/notes.py

--- notes/note_record.py
+++ notes/note_record.py
@@ -1,19 +1,24 @@
-from class_fields.name import Name
+import datetime
 
 
-class NoteRecord():
-    def __init__(self, name: Name, record: str):
-        self.name = name
-        self.record = record
+class NoteRecord:
+    def __init__(self, author, text, tags, date=None):
+        self.author = author
+        self.text = text
+        self.tags = tags
+        self.date = date if date else datetime.datetime.now()
 
-    def add_teg(self, teg):
-        self.teg = teg    
+    # зміна тексту нотатки
+    def update_text(self, new_text):
+        self.text = new_text
 
-    def __str__(self):
-        return f"Title : {self.name.value}, record: {self.record}"
+    # зміна автора
+    def update_author(self, new_author):
+            self.author = new_author
 
-    def __repr__(self):
-        return f"Title : {self.name.value}, record: {self.record}" 
+    # зміна тегу
+    def update_tags(self, new_tags):
+        self.tags = new_tags
 
 
          
--- notes/notes.py
+++ notes/notes.py
@@ -1,4 +1,48 @@
-from collections import UserDict
+from prettytable import PrettyTable
+from note_record import NoteRecord
 
-class Notes(UserDict):
-    def__init__
+class Notes:
+
+    # список для зберігання нотаток
+    def __init__(self):
+        self.notes = []
+
+    # додати нотатку
+    def add_note(self, author, text, tags):
+        new_note =  NoteRecord(author, text, tags)
+        self.notes.append(new_note)
+
+    # пошук за тегами
+    def search_by_tags(self, search_tags):
+        result_notes = [note for note in self.notes if set(search_tags).issubset(note.tags)]
+        if result_notes:
+            table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
+            for i, note in enumerate(result_notes):
+                table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
+            print(table)
+        else:
+            print("No notes found with the specified tags.")
+
+    # сортування за тегами по алфавіту
+    def sort_by_tags(self):
+        sorted_notes = sorted(self.notes, key=lambda note: ','.join(note.tags))
+        table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
+        for i, note in enumerate(sorted_notes):
+            table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
+        print(table)
+
+    # видалення нотатки
+    def delete_note_by_index(self, index):
+        if 0 <= index < len(self.notes):
+            del self.notes[index]
+            print("Note deleted successfully.")
+        else:
+            print("Invalid note index.")
+
+    # Виведення нотаток таблицею
+    def display_notes_table(self):
+            table = PrettyTable(['Index', 'Author', 'Text', 'Tags', 'Date'])
+            for i, note in enumerate(self.notes):
+                table.add_row([i, note.author, note.text, ', '.join(note.tags), note.date.strftime('%Y-%m-%d %H:%M:%S')])
+
+            print(table)
