b3947fac39945b39c1e8fd39256ed2cd7894ed7a
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2023-12-20 00:41:10 +0200

    fix4

--- user_assistant/handlers/address_book/search_contact.py
+++ user_assistant/handlers/address_book/search_contact.py
@@ -1,2 +1,16 @@
+#  TODO Add formatted output
 def search_contact(book):
-    pass
+    while True:
+        string = input(f'Input keyword for name or phone :')
+        if string:
+            break
+        print('Value is missing, try agane')
+
+    result = book.search(string)
+
+    if result:
+        for record in result:
+            print(record)
+         
+    else:
+        print(f'There is no any contact with keyword: {string}')
