de93c0f72357757092a810a22d32a17b7657b794
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2023-12-20 12:40:22 +0200

    ed1

--- user_assistant/handlers/address_book/show_all_contacts.py
+++ user_assistant/handlers/address_book/show_all_contacts.py
@@ -1,7 +1,6 @@
-from user_assistant.address_book.address_book import AddressBook
 from user_assistant.console.console import Console
 
-def show_all_contacts(book: AddressBook):
+def show_all_contacts(book):
     for value in book.data.values():
         Console.print_success(value)
          
