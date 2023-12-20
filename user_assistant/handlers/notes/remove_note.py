b8b3b88203a365eaadc29d9a450b1e810c8e3357
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2023-12-20 21:35:11 +0200

    f2

--- user_assistant/handlers/notes/remove_note.py
+++ user_assistant/handlers/notes/remove_note.py
@@ -1,2 +1,13 @@
-def remove_note():
-    pass
+from user_assistant.notes.notes import Notes
+from user_assistant.console.console import Console
+from user_assistant.console.table_format.notes_table import note_titles, get_notes_row
+
+def remove_note(notes: Notes):
+    value = Console.input('Input name or teg :')
+    result  = notes.find(value)
+
+    if result is not None:
+        result.delete(value)
+        return Console.print_table(f'Remove note', note_titles, [get_notes_row(result)])
+        
+     
