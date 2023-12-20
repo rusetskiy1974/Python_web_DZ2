9ecc3f89878a90ae4df9b478b117c0b15b904599
retOK1986 <retOK@i.ua>
2023-12-20 18:09:06 +0200

    creating logic for adding a tag to a note

--- user_assistant/handlers/notes/add_tags.py
+++ user_assistant/handlers/notes/add_tags.py
@@ -1,2 +1,20 @@
-def add_tags():
-    pass
+from user_assistant.notes.notes import Notes
+from user_assistant.console.console import Console
+from user_assistant.class_fields.tag import Tag
+from user_assistant.handlers.input_value import input_value
+
+def add_tags(notes: Notes):
+    note_id = Console.input("Введіть ID нотатки, до якої потрібно додати теги: ")
+
+    note = notes.find(note_id)
+    if note is None:
+        return Console.print_error(f"Немає нотатки з ID: {note_id}")
+
+    while True:
+        tag_value = input_value("tag", Tag)
+        note.add_tag(Tag(tag_value))
+
+        if Console.input("Додати ще один тег? (так/ні) ") != "так":
+            break
+
+    Console.print_success(f"Теги успішно додані до нотатки {note_id}")
