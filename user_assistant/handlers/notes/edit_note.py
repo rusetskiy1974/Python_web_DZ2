1e9f55a935bf09fd822d6647a47e4f76447d1b08
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2023-12-21 16:02:54 +0200

    f2

--- user_assistant/handlers/notes/edit_note.py
+++ user_assistant/handlers/notes/edit_note.py
@@ -5,40 +5,31 @@ from user_assistant.class_fields.text import Text
 from user_assistant.notes.notes import Notes
 from user_assistant.console.console import Console
 from user_assistant.storages.storage import Storage
+from user_assistant.handlers.input_value import input_value
 from user_assistant.console.table_format.notes_table import note_titles, get_notes_row
 
 
-NOTES_FIELDS = ['author', 'text']
+NOTES_CLASS = {'author': Author, 'text': Text}
 
 def edit_note(notes: Notes, storage: Type[Storage]):
     Console.print_tip('Press “Enter” with empty value to skip')
     while True:
         value_id = Console.input(f'Enter note ID: ')
         existing_note = notes.find(value_id)
-        print(type(existing_note))
+        
         if existing_note:
             break
         else:
             Console.print_error('Input existing ID')
 
-    while True:
-        for field in NOTES_FIELDS:
-            # new_volume = Console.input(f'Input new value for {field}: ')
+    for field in NOTES_CLASS:
+        new_volume = input_value(f'new value for {field}', NOTES_CLASS[field], True) 
 
-            if field == 'author':
-                try:
-                    existing_note.edit_author(Author(Console.input(f'Input new value for {field}: ')))
-                except Exception as error:
-                    Console.print_error(error)
-                    break    
+        if field == 'author' and new_volume:
+            existing_note.edit_author(new_volume)
 
-            if field == 'text':
-                try:
-                    existing_note.edit_text(Text(Console.input(f'Input new value for {field}: ')))
-                except Exception as error:
-                    Console.print_error(error)
-                    break 
-        break       
-    
+        if field == 'text' and new_volume:
+            existing_note.edit_text(new_volume)
+                 
     Console.print_table(f'Selected note chahged', note_titles, [get_notes_row(existing_note)])
     storage.update(notes.data.values())
