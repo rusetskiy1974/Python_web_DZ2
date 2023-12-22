2f4fdbbd6b36c3df8e2c70bb06d5630ab8d132e1
PotapovALeksey <37796979+PotapovALeksey@users.noreply.github.com>
2023-12-22 09:40:03 +0200

    fixed id field

--- user_assistant/class_fields/id.py
+++ user_assistant/class_fields/id.py
@@ -4,4 +4,4 @@ from datetime import datetime
 
 class ID(Field):
     def __init__(self, init_id=None):
-        super().__init__(init_id if init_id is not None else datetime.timestamp(datetime.now()))
+        super().__init__(init_id if init_id is not None else str(datetime.timestamp(datetime.now())))
