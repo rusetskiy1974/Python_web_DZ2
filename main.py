f267f945880bdcd80d94950a731739afa23b5be9
PotapovALeksey <37796979+PotapovALeksey@users.noreply.github.com>
2023-12-22 13:29:21 +0200

    asfdds

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from user_assistant.main import main
-import re
-
-
-str = str(Exception('The date is incorrect. The format should be [12.10.1994]'))
-
-res = re.findall(r'\[.*\]', str)[0]
-updated_format = re.sub("[\[\]]","", res)
-new_str = str.replace(res, f'[bold]{updated_format}[/]')
 
 if __name__ == '__main__':
     main()
