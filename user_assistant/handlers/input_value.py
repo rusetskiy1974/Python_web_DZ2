784288246eb311eaf8e7f189e774ea9282af984a
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2023-12-23 00:01:44 +0200

    Edit_note_contact_old_value_text

--- user_assistant/handlers/input_value.py
+++ user_assistant/handlers/input_value.py
@@ -1,9 +1,23 @@
 from user_assistant.console.console import Console
+import keyboard
+import pyperclip
+
+def write(text):
+    pyperclip.copy(text)  
+    pyperclip.paste()
 
 
-def input_value(value: str, class_field: classmethod, is_edit=False, placeholder=None):
+def input_value(value: str, class_field: classmethod, is_edit=False, old_value = None, placeholder=None):
     while True:
-        result = Console.input(f'Enter {value}: ', placeholder=placeholder).strip()
+        if old_value:
+            print(f'Change old {value} :', end='')
+            write(old_value)
+            keyboard.press('ctrl+v') 
+            keyboard.release('ctrl+v')
+            result = input()
+            pyperclip.copy('')
+        else:    
+            result = Console.input(f'Enter {value}: ', placeholder=placeholder).strip()
         if result == '' and is_edit:
             return result
         try:
--- user_assistant/handlers/notes/edit_note.py
+++ user_assistant/handlers/notes/edit_note.py
@@ -28,7 +28,13 @@ def edit_note(notes: Notes, storage: Type[Storage]):
     Console.print_tip('Press “Enter” with empty value to skip')
 
     for field in NOTES_CLASS:
-        new_volume = input_value(f'new value for {field}', NOTES_CLASS[field], True) 
+
+        if field == 'author':
+            volume = str(existing_note.author)
+        if field == 'text':  
+            volume = str(existing_note.text)  
+
+        new_volume = input_value(f'new value for {field}', NOTES_CLASS[field], True, old_value=volume) 
 
         if field == 'author' and new_volume:
             existing_note.edit_author(new_volume)
