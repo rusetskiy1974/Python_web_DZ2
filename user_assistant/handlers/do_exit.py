b0e95a35a1ec1bffbd59ae03fb1e59f0a4dd95da
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2024-01-07 22:30:26 +0200

    fixx

--- user_assistant/handlers/do_exit.py
+++ user_assistant/handlers/do_exit.py
@@ -3,3 +3,4 @@ from user_assistant.console.console import Console
 
 def do_exit():
     Console.print(":waving_hand:[plum3] Good luck. Hope we will see you soon[/]")
+    return exit()
--- user_assistant/handlers/help.py
+++ user_assistant/handlers/help.py
@@ -1,43 +1,44 @@
 from user_assistant.console.console import Console
-import user_assistant.commands as COMMANDS
+
 
 column_titles = ['Command', 'Fields', 'Description']
 
+
 address_book_documentation = [
-    [COMMANDS.ADD_CONTACT, 'name, birthday, phone, address, mail', 'Create a new contact'],
-    [COMMANDS.EDIT_CONTACT, 'name, birthday, address, mail', 'Edit an existing contact'],
-    [COMMANDS.REMOVE_CONTACT, 'name', 'Remove an existing contact'],
-    [COMMANDS.FIND_CONTACT, 'name', 'Find a contact by name'],
-    [COMMANDS.ADD_PHONE, 'name, phone', 'Add a new phone for contact'],
-    [COMMANDS.EDIT_PHONE, 'name', 'Edit an existing phone of contact'],
-    [COMMANDS.REMOVE_PHONE, 'name', 'Remove an existing phone of contact'],
-    [COMMANDS.SEARCH_CONTACTS, 'name or phone', 'Search contacts by name or phone'],
-    [COMMANDS.SHOW_ALL_CONTACTS, '', 'Show all contacts'],
-    [COMMANDS.SHOW_BIRTHDAY, 'days', 'Show contacts who birthdays will be in period of entered days'],
+    ['add_contact', 'name, birthday, phone, address, mail', 'Create a new contact'],
+    ['edit_contact', 'name, birthday, address, mail', 'Edit an existing contact'],
+    ['remove_contact', 'name', 'Remove an existing contact'],
+    ['find_contact', 'name', 'Find a contact by name'],
+    ['add_phone', 'name, phone', 'Add a new phone for contact'],
+    ['edit_phone', 'name', 'Edit an existing phone of contact'],
+    ['remove_phone', 'name', 'Remove an existing phone of contact'],
+    ['search_contacts', 'name or phone', 'Search contacts by name or phone'],
+    ['show_all_contacts', '', 'Show all contacts'],
+    ['show_birthday',  'days', 'Show contacts who birthdays will be in period of entered days'],
 ]
 
 notes_documentation = [
-    [COMMANDS.ADD_NOTE, 'author, text, tag', 'Create a new note'],
-    [COMMANDS.EDIT_NOTE, 'id, author, text', 'Edit an existing note'],
-    [COMMANDS.REMOVE_NOTE, 'id', 'Remove an existing contact'],
-    [COMMANDS.FIND_NOTE, 'id', 'Find a note by id'],
-    [COMMANDS.ADD_TAGS, 'id, tags', 'Add tags of note by id'],
-    [COMMANDS.REMOVE_TAGS, 'id, tags', 'Remove tags of note by id'],
-    [COMMANDS.SHOW_ALL_NOTES, '', 'Show all notes'],
-    [COMMANDS.SEARCH_NOTES_BY_TAG, 'tag', 'Search notes by tag'],
-    [COMMANDS.SEARCH_NOTES_BY_AUTHOR, 'author', 'Search notes by author'],
-    [COMMANDS.SORT_NOTES_BY_TAGS, '', 'Sort notes by tags'],
-    [COMMANDS.SORT_NOTES_BY_AUTHOR, '', 'Sort notes by author'],
+    ['add_note', 'author, text, tag', 'Create a new note'],
+    ['edit_note', 'id, author, text', 'Edit an existing note'],
+    ['remove_note', 'id', 'Remove an existing contact'],
+    ['find_note', 'id', 'Find a note by id'],
+    ['add_tags', 'id, tags', 'Add tags of note by id'],
+    ['remove_tags', 'id, tags', 'Remove tags of note by id'],
+    ['show_all_notes', '', 'Show all notes'],
+    ['search_notes_by_tag', 'tag', 'Search notes by tag'],
+    ['search_notes_by_author', 'author', 'Search notes by author'],
+    ['sort_notes_by_tags', '', 'Sort notes by tags'],
+    ['sort_notes_by_author', '', 'Sort notes by author'],
 ]
 
 sort_files_documentation = [
-    [COMMANDS.SORT_FILES, 'folder', 'Sorting files inside folder by categories: music, image, video, documents'],
+    ['sort_files', 'folder', 'Sorting files inside folder by categories: music, image, video, documents'],
 ]
 
 additional_commands_documentation = [
-    [COMMANDS.EXIT, 'Exit from user assistant'],
-    [COMMANDS.CLOSE, 'Exit from user assistant'],
-    [COMMANDS.HELP, 'Show documentations'],
+    ['exit', 'Exit from user assistant'],
+    ['close', 'Exit from user assistant'],
+    ['help', 'Show documentations'],
 ]
 def help():
     Console.print_table('Address book', column_titles, address_book_documentation)
