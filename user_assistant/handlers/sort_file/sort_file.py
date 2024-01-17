9a21691a9508601d1cb4c016ba268fa7a928e70c
SergRusetskiy1974 <113336060+rusetskiy1974@users.noreply.github.com>
2024-01-17 23:12:54 +0200

    fix

--- user_assistant/handlers/sort_file/sort_file.py
+++ user_assistant/handlers/sort_file/sort_file.py
@@ -6,9 +6,10 @@ from user_assistant.console.console import Console
 def sort_files():
     try:
         folder_for_scan = Console.input('Enter folder for scan: ')
-        sorter = FileSorter(Path(folder_for_scan))
-        sorter.sort_files()
+        if folder_for_scan:
+            sorter = FileSorter(Path(folder_for_scan))
+            sorter.sort_files()
 
-        Console.print_error('The directory was sorted.')
+            Console.print_error('The directory was sorted.')
     except (FileNotFoundError, OSError) as e:
         Console.print_error(f'Error: {e}')
